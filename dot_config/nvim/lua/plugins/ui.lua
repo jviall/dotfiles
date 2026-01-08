2d7b7c7c2d91564cf08d8ccb6411a76f09027e04
jviall <jviall@pm.me>
2026-01-08 10:29:05 -0800

    fix(nvim): correct markdown-preview config

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -107,25 +107,25 @@ return {
     opts = {
       defaults = {},
     },
-    {
-      "iamcco/markdown-preview.nvim",
-      cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-      build = function()
-        require("lazy").load({ plugins = { "markdown-preview.nvim" } })
-        vim.fn["mkdp#util#install"]()
-      end,
-      keys = {
-        {
-          "<leader>cp",
-          ft = "markdown",
-          "<cmd>MarkdownPreviewToggle<cr>",
-          desc = "Markdown Preview",
-        },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
       },
-      config = function()
-        vim.cmd([[do FileType]])
-      end,
     },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
   },
   {
     "MeanderingProgrammer/render-markdown.nvim",
