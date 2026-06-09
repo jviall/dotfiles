a9337dd1bd5412ae203002b89f959296d358a187
jviall <jviall@pm.me>
2026-06-08 21:42:24 -0700

    feat: Powershell profile agnostic of version

---
+++ dot_config/powershell/profile.ps1
@@ -0,0 +1,48 @@
+# XDG-equivalent environment variables
+$env:XDG_CONFIG_HOME = "$HOME/.config"
+$env:XDG_DATA_HOME   = "$HOME/.local/share"
+$env:XDG_CACHE_HOME  = "$HOME/.cache"
+$env:PATH            = "$HOME/.config/bin" + [IO.Path]::PathSeparator + $env:PATH
+
+# Git aliases
+function gs    { git status }
+function ga    { git add @args }
+function gaa   { git add -A }
+function gap   { git add -p }
+function gd    { git diff @args }
+function gdc   { git diff --cached }
+function gst   { git stash @args }
+function gch   { git checkout @args }
+function gnb   { git checkout -b @args }
+function gpull { git pull }
+function gc    { git commit -S @args }
+function gcm   { git commit -S -m @args }
+function gca   { git commit -S -a @args }
+function gcam  { git commit -S -am @args }
+function gce   { git commit -S --amend @args }
+function gl {
+    git log --pretty=format:'%C(yellow)%h%Creset %ad %C(cyan)%an%Creset: %s' --date=relative -n 10
+}
+function gll {
+    git log --graph --name-status --pretty=format:"%C(red)%h %C(reset)(%cd) %C(green)%an %Creset%s %C(yellow)%d%Creset" --date=relative
+}
+function g {
+    if ($args.Count -eq 0) { git status } else { git @args }
+}
+
+# Eza (modern ls replacement)
+if (Get-Command eza -ErrorAction SilentlyContinue) {
+    function ls  { eza -ax --icons @args }
+    function ll  { eza -albh --icons --git @args }
+    function lr  { eza -TL 3 --icons @args }
+}
+
+# Zoxide (smarter cd with z alias)
+if (Get-Command zoxide -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (zoxide init powershell | Out-String) })
+}
+
+# Starship prompt (requires Starship installed via winget)
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+    Invoke-Expression (&starship init powershell)
+}
